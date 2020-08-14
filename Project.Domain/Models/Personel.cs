a861add02dbf95eda26c76a1efe2578ad71d856b
Ayşenur Yıldız <aysenuryildiz10@gmail.com>
2020-08-14 22:18:58 +0300

    repository oluşturuldu. personel tablosu eklendi

---
+++ Project.Domain/Models/Personel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project.Domain
+{
+    [Table("Personel")]
+    public class Personel : BaseDataEntities
+    {
+        [Column("Name")]
+        public string Name { get; set; }
+
+        [Column("LastName")]
+        public string LastName { get; set; }
+
+    }
+}
---
+++ Project.Domain/Services/PersonelDataService.cs
@@ -0,0 +1,11 @@
+﻿namespace Project.Domain
+{
+    public sealed class PersonelDataService : BaseDataServices<Personel>
+    {
+        public int InsertCommand(Personel personel)
+        {
+            string query = "INSERT INTO Personel VALUES (@Name,@LastName)";
+            return ExecuteCommandText(query,p);
+        }
+    }
+}
